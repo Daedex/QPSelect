170e5c932b7db340ce6c3e05bcb7653a11d45394
ma77ken7 <ma77ken7@gmail.com>
2015-02-28 17:56:17 -0500

    more updates

--- master.js
+++ master.js
@@ -1,59 +1,62 @@
-//Run Selection
+//run the current selection
 function runSelection() {
 		var airVolume = $('#airVolume').val();
-		var sectionWidth = $('#sectionWidth').val();
-		var sectionHeight = $('#sectionHeight').val();
-
-		var airVelocity = airVolume/((sectionWidth*sectionHeight)/144);
+		var airVolume = $('#totalStaticPressure').val();
+		var unitWidth = $('#unitWidth').val();
+		var unitHeight = $('#unitHeight').val();
 		
-		$('#totalStaticPressure').val(airVelocity); 
-
+		//run createGrid function 
+		createGrid();
 	};
 
+//clear the current selection
 function clearSelection() {
 		$('#airVolume').val(null);
 		$('#totalStaticPressure').val(null);
-		$('#sectionWidth').val(null);
-		$('#sectionHeight').val(null);
+		$('#powerSupply').val(null);
+		$('#unitWidth').val(null);
+		$('#unitHeight').val(null);
 	};
 
-	/*   ** GRID AS A TABLE **   
-			$(document).ready(function() {
-				var $table = $('#pixelgrid');
-				var html = [];
-				var row, col;
-				for(row=0; row<80; row++) {
-					html.push('<tr class="line">')
-					for(col=0; col<80; col++) {
-						html.push('<td class="square">'+'</td>');
-					}
-					html.push('</tr>');
-				}
-				$table.append(html.join(''));
-
-				//document.querySelector(".square").onclick=function(){
-					// document.querySelector(".square").style.backgroundColor="red"; 
-			//}
-			});
-		
-
-
-		/** GRID USING DIVS ** // 
-		var $grid = $('#pixelGrid');
-		for(i=0; i<100; i++) {
-
-			var row = '<div>';
-
-			for(j=0; j<100; j++) {
-				row += '<div class = "square">' + '</div>';
+//create grid for current selection
+function createGrid() {  	
+		var $table = $('#selectGrid');
+		var html = [];
+		var row, col;
+		for(row=0; row<80; row++) {
+			html.push('<tr class="line">')
+			for(col=0; col<80; col++) {
+				html.push('<td class="square">'+'</td>');
 			}
-
-			row += '</div>';
-
-			$grid.append(row);
+			html.push('</tr>');
 		}
+		$table.append(html.join(''));
 
-		document.getElementsByClassName('.square').onlick=function() {
-			document.getElementsByClassName('.square').style.backgroundColor="red";
+/*
+		document.querySelector(".square").onclick=function(){
+			document.querySelector(".square").style.backgroundColor="red"; 
 		}
-		*/
+*/
+	};
+
+
+
+	/** GRID USING DIVS ** // 
+	var $grid = $('#pixelGrid');
+	for(i=0; i<100; i++) {
+
+	var row = '<div>';
+
+	for(j=0; j<100; j++) {
+		row += '<div class = "square">' + '</div>';
+	}
+
+	row += '</div>';
+
+	$grid.append(row);
+	}
+
+	document.getElementsByClassName('.square').onlick=function() {
+	document.getElementsByClassName('.square').style.backgroundColor="red";
+	}
+	*/
