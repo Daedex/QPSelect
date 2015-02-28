1f3134ea1b4ccf2bc18465389e4b0065b8fec8c5
ma77ken7 <ma77ken7@gmail.com>
2015-02-28 16:46:17 -0500

    major changes, creating external js functions

--- master.js
+++ master.js
@@ -1,2 +1,11 @@
-//create function to get input text box values
-document.getElementById("searchTxt").value;
+//Run Selection
+function runSelection() {
+		var airVolume = $('#airVolume').val();
+		var sectionWidth = $('#sectionWidth').val();
+		var sectionHeight = $('#sectionHeight').val();
+
+		var airVelocity = airVolume/((sectionWidth*sectionHeight)/144);
+		
+		$('#totalStaticPressure').val(airVelocity); 
+
+	};
