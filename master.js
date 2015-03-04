ab16c4b9d807dbc3b4f9f068ca90ac6e3a65095d
ma77ken7 <ma77ken7@gmail.com>
2015-03-04 16:16:41 -0500

    updates

--- master.js
+++ master.js
@@ -1,51 +1,50 @@
 //square class declaration
-function Square(unitWidth, unitHeight){
-	this.width=unitWidth;
-	this.height=unitHeight; 
-}
+var Unit = function (width, height) {
+	this.width = width;
+	this.height = height;
+};
 
 //run the current selection
 function runSelection() {
-		var airVolume = $('#airVolume').val();
-		var airVolume = $('#totalStaticPressure').val();
-				
-		//run createGrid function 
-		createGrid();
-	};
+	var airVolume = $('#airVolume').val();
+					
+	//run createGrid function 
+	createGrid();
+};
 
 //clear the current selection
 function clearSelection() {
-		$('#airVolume').val(null);
-		$('#totalStaticPressure').val(null);
-		$('#powerSupply').val(null);
-		$('#unitWidth').val(null);
-		$('#unitHeight').val(null);
-		createGrid();
-	};
+	$('#airVolume').val(null);
+	$('#totalStaticPressure').val(null);
+	$('#powerSupply').val(null);
+	$('#unitWidth').val(null);
+	$('#unitHeight').val(null);
+	createGrid();
+};
 
 //create grid for current selection
 function createGrid() {  
-		$('#grid').empty();
-		var unitWidth = $('#unitWidth').val();
-		var unitHeight = $('#unitHeight').val();
-		var $grid = $('#grid');
-		var html = [];
-		var W, H;
-		for(H=182; H>=24; H-=2) {
-			html.push('<tr class="line">')
-			for(W=24; W<=182; W+=2) {
-				if(W==unitWidth && H==unitHeight) {
-					html.push('<td class="selectedSquare">'+'</td>');
-				}
-				else {
-
-					html.push('<td class="square" id="square'+W+'x'+H+'" onclick="clickSquare();">'+'</td>');
-				}
+	$('#grid').empty();
+	var unitWidth = $('#unitWidth').val();
+	var unitHeight = $('#unitHeight').val();
+	var $grid = $('#grid');
+	var html = [];
+	var W, H;
+	for(H=182; H>=24; H-=2) {
+		html.push('<tr class="line">')
+		for(W=24; W<=182; W+=2) {
+			if(W==unitWidth && H==unitHeight) {
+				html.push('<td class="selectedSquare">'+'</td>');
 			}
-			html.push('</tr>');
+			else {
+
+				html.push('<td class="square" id="square'+W+'x'+H+'" onclick="clickSquare();">'+'</td>');
+			}
 		}
-		$grid.append(html.join(''));
-	};
+		html.push('</tr>');
+	}
+	$grid.append(html.join(''));
+};
 
 //makes clicked square the selected square
 function clickSquare() {
