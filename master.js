bb9fd7a13e0d4fa5f9bb0f3a0278fd0c575da5c3
ma77ken7 <ma77ken7@gmail.com>
2015-03-06 13:38:21 -0500

    updates

--- master.js
+++ master.js
@@ -2,7 +2,7 @@
 function initGrid() {
 	var $grid = $('#grid');
 	var html = [];
-	var id, x, y,
+	var x, y, id, 
 		X_MIN = 36,
 		Y_MIN = 26,
 		X_MAX = 228,
@@ -11,8 +11,8 @@ function initGrid() {
 	for(y=Y_MAX; y>=Y_MIN; y-=2) {
 	    html.push('<tr>')
 	    for(x=X_MIN; x<=X_MAX; x+=2) {
-	    	id = '"'+'x'+x+'y'+y+'"';
-	        html.push('<td class="selection-type-0" id='+id+'></td>');
+	    	id = 'x'+x+'y'+y;
+	        html.push('<td class="selection-type-0" id="'+id+'"" onclick="clickSelection(this.id);"></td>');
 	        //initialize Selection objects here??
 	    }
 	    html.push('</tr>');
@@ -21,8 +21,8 @@ function initGrid() {
 
 	//unavailable selection sizes - will we need to hardcode all of them?
 	//i think a couple for loops could do the trick
-	document.getElementById("x36y26").className = "selection-type-none";
-	document.getElementById("x228y130").className = "selection-type-none";
+	//document.getElementById("x36y26").className = "selection-type-none";
+	//document.getElementById("x228y130").className = "selection-type-none";
 };
 
 //refreshes the grid
@@ -33,32 +33,77 @@ function refreshGrid() {
 	initGrid();
 };
 
+function inputSelection() {
+	//retreive and define dimensions from input fields
+	var x = $('#unitWidth').val();
+	var y = $('#unitHeight').val();
+	//create and define id string
+	var id = 'x'+x+'y'+y;
+	runSelection(id);
+};
+
+function clickSelection(id) {
+	var id = id,
+		arrayId = parseId(id);
+	alert(parseInt('x36'));
+	$('#unitWidth').val(arrayId[0]);
+	$('#unitHeight').val(arrayId[1]);
+	runSelection(id);
+};
+
 //run the current selection
-function runSelection() {
+function runSelection(id) {
 	//refresh grid to remove previous selections
-	refreshGrid();
-	//store dimensions
-	var width = $('#width').val();
-	var height = $('#height').val();
-	//create id string - this is redundant and will be part of the selection class
-	var size = 'x'+width+'y'+height;
+	refreshGrid();	
 	//renames the table element's class name to indicate selected size with css
-	document.getElementById(size).className = "selected";
+	document.getElementById(id).className = "selected";
+};
+
+function parseId(id) {
+	var x = 60,
+		y = 60;
+	return [x, y];
 };
 
 //clear the current selection
 function clearSelection() {
 	//sets inputs to null
-	$('#width').val(null);
-	$('#height').val(null);
+	$('#unitWidth').val(null);
+	$('#unitHeight').val(null);
 	//refresh grid to remove previous selections
 	refreshGrid();
 };
 
+function getre(str, num) {
+  if(str === num) return 'nice try';
+  var res = [/^\D+/g,/\D+$/g,/^\D+|\D+$/g,/\D+/g,/\D.*/g, /.*\D/g,/^\D+|\D.*$/g,/.*\D(?=\d)|\D+$/g];
+  for(var i = 0; i < res.length; i++)
+    if(str.replace(res[i], '') === num) 
+      return 'num = str.replace(/' + res[i].source + '/g, "")';
+  return 'no idea';
+};
+function update() {
+  $ = function(x) { return document.getElementById(x) };
+  var re = getre($('str').value, $('num').value);
+  $('re').innerHTML = 'Numex speaks: <code>' + re + '</code>';
+}
 
+
+
+
+
+
+/*
 //selection class declaration
-var Selection = function (x, y) {
+var Selection = function (x, y, id) {
 	this.width = x;
 	this.height = y;
-	this.id = '"'+'x'+x+'y'+y+'"';
+	this.id = id;
+
+	
+	Selection.prototype.info () {
+		return '<p>This is some info on this selection</p>'
+	};
+	
 };
+*/
