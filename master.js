37e8f410f16145a625d017f9aa65dabc6444da8d
ma77ken7 <ma77ken7@gmail.com>
2015-03-06 02:21:13 -0500

    making a lot of progress, cant believe it is 2:30am right now

--- master.js
+++ master.js
@@ -1,63 +1,69 @@
-function myAlert() {
-	alert("please work");
-};
-/*
-//selection class declaration
-var Selection = function () {
-	this.width = width;
-	this.height = height;
+//initialize the grid
+function initGrid() {
+	var $grid = $('#grid');
+	var html = [];
+	var id, x, y,
+		X_MAX = 182,
+		Y_MAX = 182;
+		
+	for(y=Y_MAX; y>=24; y-=2) {
+	    html.push('<tr>')
+	    for(x=24; x<=X_MAX; x+=2) {
+	    	id = '"'+'x'+x+'y'+y+'"';
+	        html.push('<td class="selection-type-0" id='+id+'></td>');
+	        //initialize Selection objects here??
+	    }
+	    html.push('</tr>');
+	}
+	$grid.append(html.join(''));
 
+	//unavailable selection sizes - will we need to hardcode all of them?
+	//i think a couple for loops could do the trick
+	document.getElementById("x182y182").className = "selection-type-none";
+	document.getElementById("x180y182").className = "selection-type-none";
+	document.getElementById("x182y180").className = "selection-type-none";
+	document.getElementById("x180y180").className = "selection-type-none";
+	document.getElementById("x178y182").className = "selection-type-none";
+	document.getElementById("x178y180").className = "selection-type-none";
+	document.getElementById("x178y178").className = "selection-type-none";
+	document.getElementById("x180y178").className = "selection-type-none";
+	document.getElementById("x182y178").className = "selection-type-none";
+};
+
+//refreshes the grid
+function refreshGrid() {
+	//delete all contents of the table "grid"
+	$('#grid').empty();
+	//reinitialize the grid
+	initGrid();
 };
 
 //run the current selection
 function runSelection() {
-	alert("test");
+	//refresh grid to remove previous selections
+	refreshGrid();
+	//store dimensions
+	var width = $('#width').val();
+	var height = $('#height').val();
+	//create id string - this is redundant and will be part of the selection class
+	var size = 'x'+width+'y'+height;
+	//renames the table element's class name to indicate selected size with css
+	document.getElementById(size).className = "selected";
 };
 
 //clear the current selection
 function clearSelection() {
+	//sets inputs to null
 	$('#width').val(null);
 	$('#height').val(null);
+	//refresh grid to remove previous selections
+	refreshGrid();
 };
 
-//initialize the grid
-function initGrid() {
-	alert("test");
-	var $grid = $('#grid');
-	var html = [];
-	var x, y,
-		X_MAX = 80;
-		Y_MAX = 80;
-	for(y=0; y<=Y_MAX; y++) {
-		html.push('<tr>')
-		for(x=0; x<=X_MAX; x++) {
-				html.push('<td class="selection-type-0" id="x0y0" onclick="clickSquare();">'+'</td>');
-			}
-		}
-		html.push('</tr>');
-	}
-	$grid.append(html.join(''));
-};
 
-//create grid for current selection
-function createGrid() {  
-	$('#grid').empty();
-	var unitWidth = $('#unitWidth').val();
-	var unitHeight = $('#unitHeight').val();
-	var $grid = $('#grid');
-	var html = [];
-	var W, H;
-	for(H=182; H>=24; H-=2) {
-		html.push('<tr class="line">')
-		for(W=24; W<=182; W+=2) {
-			if(W==unitWidth && H==unitHeight) {
-				html.push('<td class="selectedSquare">'+'</td>');
-			}
-			else {
-				html.push('<td class="square" id="square'+W+'x'+H+'" onclick="clickSquare();">'+'</td>');
-			}
-		}
-		html.push('</tr>');
-	}
-	$grid.append(html.join(''));
+//selection class declaration
+var Selection = function (x, y) {
+	this.width = x;
+	this.height = y;
+	this.id = '"'+'x'+x+'y'+y+'"';
 };
