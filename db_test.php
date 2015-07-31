a10f270c9bd3ead277c47085d1d41a50cdd07a65
Dustin Burt <dburt88@gmail.com>
2015-07-31 12:18:34 -0400

    populatin' that shitttt

--- db_test.php
+++ db_test.php
@@ -1,4 +1,6 @@
 <?php
+$first_name = mysql_real_escape_string($_POST["first_name"]);
+$last_name = mysql_real_escape_string($_POST["last_name"]);
 $email = mysql_real_escape_string($_POST["email"]);
 $Database = "quips";
 mysql_connect("localhost", "root", "Quips321") or die(mysql_error());
--- login.php
+++ login.php
@@ -1,3 +1,11 @@
 <?php
-	echo "Testing";
+$first_name = mysql_real_escape_string($_POST["first_name"]);
+$last_name = mysql_real_escape_string($_POST["last_name"]);
+$email = mysql_real_escape_string($_POST["email"]);
+$Database = "quips";
+mysql_connect("localhost", "root", "Quips321") or die(mysql_error());
+mysql_select_db($Database) or die(mysql_error());
+$insert = "INSERT INTO user (first_name, last_name, email) 
+	       VALUES ('$first_name', '$last_name', '$email')"; 
+mysql_query($insert); 
 ?>
