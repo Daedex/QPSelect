1378b088953cb24508357c4aaf97c84ff057666d
Dustin Burt <dburt88@gmail.com>
2015-08-01 20:53:17 -0400

    changes

--- login.php
+++ login.php
@@ -1,12 +1,14 @@
 <?php
+header('Location: http://localhost/quips/index.html');
 $first_name = mysql_real_escape_string($_POST["first_name"]);
 $last_name = mysql_real_escape_string($_POST["last_name"]);
 $email = mysql_real_escape_string($_POST["email"]);
+$user_name = mysql_real_escape_string($_POST["user_name"]);
 $password = mysql_real_escape_string($_POST["password"]);
 $Database = "quips";
 mysql_connect("localhost", "root", "Quips321") or die(mysql_error());
 mysql_select_db($Database) or die(mysql_error());
-$insert = "INSERT INTO user (first_name, last_name, email, password) 
-	       VALUES ('$first_name', '$last_name', '$email', '$password')"; 
+$insert = "INSERT INTO user (first_name, last_name, email, user_name, password)
+	       VALUES ('$first_name', '$last_name', '$email', '$user_name', '$password')"; 
 mysql_query($insert); 
 ?>
