9cf2b225bb9b79c6970e0a39c952a15d0728bf3a
Dustin Burt <dburt88@gmail.com>
2015-08-18 08:02:18 -0400

    merge

--- create_job.php
+++ create_job.php
@@ -1,10 +1,13 @@
 <?php
-//$job_id = mysql_real_escape_string($_POST["job_id"]);
-$job_name = mysql_real_escape_string($_POST["new_job"]);
-$Database = "quips";
-mysql_connect("localhost", "root", "Quips123") or die(mysql_error());
-mysql_select_db($Database) or die(mysql_error());
-$insert = "INSERT INTO jobs (job_name)
+header('Location: http://localhost/quips/quips.html');
+$job_name = mysql_real_escape_string($_GET["new_job"]);
+$database = "quips";
+$location = "localhost";
+$user = "root";
+$pw = "Quips123"; 
+mysql_connect($location, $user, $pw) or die(mysql_error());
+mysql_select_db($database) or die(mysql_error());
+$insert = "INSERT INTO jobs (new_job)
 	       VALUES ('$job_name')"; 
 mysql_query($insert); 
 ?>
