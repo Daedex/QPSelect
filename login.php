71c09028232dd5dfcf1a45a0b2714343a03d694d
Matt Kent <ma77ken7@gmail.com>
2015-08-22 13:45:46 -0400

    updates

--- login.php
+++ login.php
@@ -1,9 +1,12 @@
 <?php
 header('Location: http://localhost/quips/');
 //connect to database
-$Database = "quips";
-mysql_connect("localhost", "root", "Quips123") or die(mysql_error());
-mysql_select_db($Database) or die(mysql_error());
+$db_host = 'daedex.com'; 
+$db_user = 'stauguu1_admin';
+$db_pwd = 'Quips123'; 
+$database = "stauguu1_quips";
+mysql_connect($db_host, $db_user, $db_pwd) or die(mysql_error());
+mysql_select_db($database) or die(mysql_error());
 
 // check if user exists, if TRUE then check for correct username and password
 $check = mysql_query("SELECT * FROM user WHERE user_name = '".$_POST['user_name']."'")or die(mysql_error());
