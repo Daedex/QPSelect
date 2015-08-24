a7361961167ff17be8d9300a86d9a2730e0c02be
Dustin Burt <dburt88@gmail.com>
2015-08-23 23:24:01 -0400

    reverted changes

--- create_job.php
+++ create_job.php
@@ -12,16 +12,4 @@ $insert = "INSERT INTO jobs (job_name)
 	       VALUES ('$new_job')"; 
 mysql_query($insert);
 
-$job_names = "SELECT * FROM jobs"; 
-$result = mysql_query($job_names) or die(mysql_error()); 
-
-while($record = mysql_fetch_array($result))
-{
-	echo "<ul>";
-	echo "<li><input name='jobID[]' id='jobID' value='" .$record['job_id']. "'>" 
-        .$record['job_name']. "</li>"; 
-	echo "</ul>"; 	
-}
-
-
 ?>
