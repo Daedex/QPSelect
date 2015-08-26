90d933e8094f9fe1c831b65e0d49bf214410f180
Matt Kent <ma77ken7@gmail.com>
2015-08-26 08:51:34 -0400

    wheww that almost sucked

--- quips.php
+++ quips.php
@@ -32,32 +32,6 @@
 				  	</div>				  		  				
 					<div class="row">					
 						<div class="col-md-2">
-<<<<<<< HEAD
-							<div class="panel panel-default">
-								<div class="panel-heading clearfix">
-									<h4 class="panel-title">Job Manager</h4>
-									<!-- Modal -->
-									<button class="btn btn-default" onclick="openModal()">New</button>
-									<div id="newJobModal" class="modal fade" style="display: none;">
-										<div class="modal-dialog">
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-														<span aria-hidden="true">x</span>
-													</button>
-													<h4 class="modal-title">New Job</h4>
-												</div>
-												<div class="modal-body">
-													<p>Job Name: </p>
-													<form method="post" action="create_job.php">
-														<input name="new_job" placeholder="New Job" autocomplete="off">
-														<input type="submit" value= "Create Job">
-													</form>
-												</div>
-												<div class="modal-footer">
-													<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-												</div>
-=======
 							<div class="panel panel-default" style="height:600px;">
 								<div class="panel-heading">
 									<h1 class="panel-title">Job Manager</h1>
@@ -83,20 +57,14 @@
 											</div>
 											<div class="modal-footer">
 												<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
->>>>>>> 86b2d48322730f78e2e7cfeda445417cf77ac9fc
 											</div>
-											<!-- modal-content -->
 										</div>
-										<!-- modal-dialog -->
+										<!-- modal-content -->
 									</div>
-									<!-- newJobModal -->
-									<input class="btn btn-default" id="del_job_btn" type="submit" action="delete_job.php" value="Delete">
+									<!-- modal-dialog -->
 								</div>
-<<<<<<< HEAD
-=======
 								<!-- newJobModal -->
 								
->>>>>>> 86b2d48322730f78e2e7cfeda445417cf77ac9fc
 								<!-- Display current jobs list -->
 								<div id="jobList">
 
@@ -135,40 +103,6 @@
 						</div>
 						
 						<div class="col-md-10">						
-<<<<<<< HEAD
-							<div class="row">
-								<div class="panel panel-default">
-									<div class="panel-heading clearfix">
-										<h4 class="panel-title pull-left">Q-PAC Selections</h4>
-										<button class="btn btn-default pull-right" onclick="">Create</button>
-									</div>
-									<table class="table table-hover table-striped table-condensed">
-										<tr>
-											<th>Unit Tag</th>
-											<th>CFM</th> 
-											<th>TSP</th>
-											<th>Voltage</th>
-											<th>Width</th>
-											<th>Height</th>
-										</tr>
-										<tr>
-											<td>AHU-1 SA</td>
-											<td>14700</td> 
-											<td>3.2</td>
-											<td>230V</td>
-											<td>120in</td>
-											<td>80in</td>
-										</tr>
-										<tr>
-											<td>AHU-1 EA</td>
-											<td>12700</td> 
-											<td>2.6</td>
-											<td>460V</td>
-											<td>140in</td>
-											<td>86in</td>
-										</tr>
-									</table>								
-=======
 							<div class="row">								
 								<div class="col-md-3">
 									<div class="panel panel-default">
@@ -234,80 +168,11 @@
 								</div>
 								<div class="col-md-5">
 									<img src="docs/fancurve2.png" class="img-rounded" alt="Fan Curve">
->>>>>>> 86b2d48322730f78e2e7cfeda445417cf77ac9fc
 								</div>
 							</div> <!-- row -->
 							<div class="row">
-								<div class="container-fluid">
+								<div class="container-fluid" id="selectionArea">
 									<ul class="nav nav-tabs">
-<<<<<<< HEAD
-										<li class="active"><a data-toggle="tab" href="#inputTab">Input</a></li>
-										<li><a data-toggle="tab" href="#resultsTab">Results</a></li>
-										<li><a data-toggle="tab" href="#outputTab">Output</a></li>
-									</ul>
-									<div class="tab-content">
-										<div id="inputTab" class="tab-pane fade in active">						
-											<form class="form-horizontal" id="selectionForm" action="select.php" method="get">
-												<div class="form-group">
-													<label class="control-label col-sm-3">Unit Tag:</label>
-													<input class="col-sm-2" id="unit_tag" autocomplete="off"></input>
-												</div>
-									  			<div class="form-group">
-													<label class="control-label col-sm-3">Air Volume:</label>
-													<input class="col-sm-2" id="air_vol" autocomplete="off">&nbsp;ft<sup>3</sup>/min</input>
-												</div>
-												<div class="form-group">
-													<label class="control-label col-sm-3">Total Static Pressure:</label>
-													<input class="col-sm-2" id="total_static_press" autocomplete="off">&nbsp;in W.C.</input>
-												</div>
-												<div class="form-group">
-													<label class="control-label col-sm-3">Voltage:</label>
-													<select class="col-sm-2" id="voltage" autocomplete="off">
-														<option value=""></option>
-														<option value="460">460V / 3~ / 60Hz</option>
-														<option value="208">208V / 3~ / 60Hz</option>
-													</select>
-												</div>
-												<div class="form-group">
-													<label class="control-label col-sm-3">Unit Width:</label>
-													<input class="col-sm-2" id="unit_weight" autocomplete="off">&nbsp;in</input>
-												</div>
-												<div class="form-group">
-													<label class="control-label col-sm-3">Unit Height:</label>
-													<input class="col-sm-2" id="unit_height" autocomplete="off">&nbsp;in</input>
-												</div>									  			
-												<div class="form-group">
-													<div class="col-sm-offset-2 col-sm-10">	
-														<input class="btn btn-default" type="submit" value="Search" id="runSelection">
-														<input class="btn btn-default" type="button" value="Reset" id="clearSelection">
-													</div>
-												</div>
-											</form>
-										</div>
-										<div id="resultsTab" class="tab-pane">
-											<table class="table table-hover table-striped table-condensed">
-												<tr>
-													<th>Quantity</th>
-													<th>Fan</th>
-													<th>HP</th>
-													<th>Efficiency</th>
-												</tr>
-												<tr>
-													<td>4</td>
-													<td>114918</td>
-													<td>4.2</td>
-													<td>1210</td>
-												</tr>
-												<tr>
-													<td>3</td> 
-													<td>114722</td>
-													<td>5.3</td>
-													<td>958</td>
-												</tr>
-											</div>
-										<div id="outputTab" class="tab-pane">
-											
-=======
 										<li class="active"><a data-toggle="tab" href="#resultsTab">Results</a></li>
 										<li><a data-toggle="tab" href="#outputsTab">Outputs</a></li>
 									</ul>
@@ -471,7 +336,6 @@
 												 </div>
 												<button type="submit" class="btn btn-default">Export</button>											
 											</form>											
->>>>>>> 86b2d48322730f78e2e7cfeda445417cf77ac9fc
 										</div>
 									</div>
 								</div>				
