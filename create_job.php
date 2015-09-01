67b2adf21883af573f8154a135ef376223c02a4d
ma77ken7 <ma77ken7@gmail.com>
2015-08-31 20:35:06 -0400

    update

--- create_job.php
+++ create_job.php
@@ -1,13 +1,10 @@
 <?php
+
+require_once('mysqli_connect.php');
+
 header('Location: http://localhost/quips/quips.php');
 $new_job = mysql_real_escape_string($_POST["new_job"]);
-$db_host = 'localhost'; 
-$db_user = 'root';
-$db_pwd = 'Quips123'; 
-$database = "quips";
 
-mysql_connect($db_host, $db_user, $db_pwd);
-mysql_select_db($database) or die(mysql_error());
 $insert = "INSERT INTO jobs (job_name)
 	       VALUES ('$new_job')"; 
 mysql_query($insert);
