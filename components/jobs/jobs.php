17c8abc0b7c57d31dab05269504c90c9ffeb2384
Dustin <dburt88@gmail.com>
2015-09-08 00:34:27 -0400

    updates

--- components/jobs/jobs.php
+++ components/jobs/jobs.php
@@ -1,35 +1,25 @@
 <?php
   
   require_once('db.php');
-
   class Job {
     public $id;
     public $name;
-
     public function __construct($id, $name){
-      this->$id = $id;
-      this->$name = $name; 
+      $this->id = $id;
+      $this->name = $name; 
     }
   }
-
   class Jobs{
     private $db; 
-
     public function __construct(){
-<<<<<<< HEAD
-      $this->db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-=======
-      $this->db = new mysqli('daedex.com', 'stauguu1_admin', 'Quips123', 'stauguu1_quips');
->>>>>>> 32951c9287b391a47cd09985c75da1ede33609a8
+      $this->db = new mysqli((DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
       $this->db->autocommit(FALSE); 
     }
-
     public function __destruct(){
       $this->db->close(); 
     }
-
     private function getID($name){
-      $stmt = $this->db->prepare("SELECT * FROM jobs WHERE job_name = ?"); 
+      $stmt = $this->db->prepare("SELECT job_id FROM jobs WHERE job_name = ?"); 
       $stmt->bind_param('s', $name); 
       $stmt->execute();
       $stmt->store_result(); 
@@ -44,44 +34,22 @@
       $id = $this->db->insert_id;
       $this->db->commit();
       $stmt->close();
-
+      
       return $id;
     }
-<<<<<<< HEAD
-
-    public function Read($id){
-      $stmt = $this->db->prepare("SELECT id, name FROM jobs WHERE id = ?");
-      $stmt->bind_param('i', $id);
-      $stmt->execute();
-      $stmt->bind_result($id, $name);
-
-      $jobs = []
-      while($stmt->fetch()) {
-        $jobs[] = new Job($id, $name);
-      }
-
-      return $jobs; 
-    }
-
-    public function Read_by_User($user_id){
-      $stmt = $this->db->prepare("SELECT id, name FROM jobs WHERE user_id = ?");
-=======
     
     public function readAll($user_id){
       $stmt = $this->db->prepare("SELECT job_id, job_name FROM jobs WHERE user_id = ?");
->>>>>>> 32951c9287b391a47cd09985c75da1ede33609a8
       $stmt->bind_param('i', $user_id); 
       $stmt->execute();
       $stmt->bind_result($id, $name);
-
+      
       $jobs = [];
       while($stmt->fetch()){
         $jobs[] = new Job($id, $name);
       }
-
       return $jobs; 
     }
-
     public function Update($name, $id){
       $stmt = $this->db->prepare("UPDATE jobs SET name = ? WHERE id = ?"); 
       $stmt->bind_param('si', $name, $id); 
@@ -89,7 +57,6 @@
       $this->db->commit(); 
       $stmt->close(); 
     }
-
     public function Delete($name, $id){
       $stmt = $this->db->prepare("DELETE FROM jobs WHERE job_name = ? AND job_id = ?");
       $stmt->bind_param('si', $name, $id); 
